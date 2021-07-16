a7d7572f72642c4f6db3d1bacf50ee09557f954e
topolian <topolian.anna@gmail.com>
2021-07-16 17:59:22 +0300

    add files to project

---
+++ utils.py
@@ -0,0 +1,29 @@
+import random
+import string
+from faker import Faker
+
+#
+# def generate_password(password_len: int = 10) -> str:
+#     if not isinstance(password_len, int):
+#         raise TypeError('Invalid Type...')
+#
+#     choices = string.ascii_letters + string.digits + '#$%^'
+#     result = ''
+#
+#     for _ in range(password_len):
+#         result += random.choice(choices)
+#
+#     return result
+
+
+def read_requirements_txt():
+    with open("requirements.txt", "r") as txt_file:
+        return txt_file.read()
+
+def making_users(users_numb=20):
+    fake = Faker()
+    user = ''
+
+    for _ in range(users_numb):
+        user += fake.name() + " " + fake.email() + " "
+    return user
